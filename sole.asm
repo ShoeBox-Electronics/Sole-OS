aed62b755036d1eaafc66224aaa4cea9757c6386
Joe P Shoulak <joe@squarecompass.io>
2024-01-13 19:25:26 -0600

    Polish and check 3x5 using repeated addition

--- sole.asm
+++ sole.asm
@@ -14,14 +14,34 @@ reset:
   jsr LCD_init
   jsr LCD_display_splash_screen
 
+  lda #3
+  sta MATH_INPUT_1
+  lda #5
+  sta MATH_INPUT_2
+  lda #0
+  sta MATH_INPUT_1 + 1
+  sta MATH_INPUT_2 + 1
+  sta MATH_OUTPUT
+  sta MATH_OUTPUT + 1
+  jsr MATH_mlt
+
+  lda MATH_OUTPUT
+  sta MATH_HEXDEC_VAL
+  lda MATH_OUTPUT + 1
+  sta MATH_HEXDEC_VAL + 1
+  jsr MATH_hexdec_convert
+
+  jsr LCD_display_hexdec_out
+
 loop:
-  jsr FIB_init
-display_loop:
-  ; calculate next fibs
-  jsr FIB_progress
-  lda FIB_LIMIT
-  beq loop
-  jmp display_loop
+  jmp loop
+;   jsr FIB_init
+; display_loop:
+;   ; calculate next fibs
+;   jsr FIB_progress
+;   lda FIB_LIMIT
+;   beq loop
+;   jmp display_loop
 
 nmi:
   ; return
